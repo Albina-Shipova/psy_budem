2cba950d90f102a6e1be410d45779175e46e5660
Albina Shipova <volare.al@gmail.com>
2022-06-25 13:26:36 +0300

    Change settings debug=true. Add localhost to allowed_hosts

--- psy_budem/budem/budem/settings.py
+++ psy_budem/budem/budem/settings.py
@@ -22,9 +22,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
 	'.xn--90agd2a6a.xn--p1ai',
 ]
 
